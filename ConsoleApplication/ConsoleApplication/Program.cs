0cc808d567ab609d9842167cbc5432abe2cd1bda
PC <PC@BARBARA-11>
2020-04-23 13:48:52 -0600

    up2

--- ConsoleApplication/ConsoleApplication/Program.cs
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -15,6 +15,24 @@ namespace ConsoleApplication {
             Console.WriteLine($"My name is: {name}");
             Console.WriteLine($"My location is: {location}");
 
+            double width, height, woodLength, glassArea;
+            Console.Write("Please enter a width in meters: ");
+            string widthString, heightString;
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
+
+            Console.Write("Please enter a height in meters: ");
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
+
+            woodLength = 2 * (width + height) * 3.25;
+            glassArea = 2 * (width * height);
+
+            Console.WriteLine("The length of the wood is " +
+            woodLength + " feet");
+            Console.WriteLine("The area of the glass is " +
+            glassArea + " square metres");
+
             Console.WriteLine("Press any key to quit");
             Console.ReadKey();
         }

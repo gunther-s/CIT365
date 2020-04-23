41a9f492799c72ca593f6b181f82ed5e5fb21289
PC <PC@BARBARA-11>
2020-04-23 13:23:32 -0600

    first

---
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "Gunther";
+            string location = "Rexburg, ID";
+
+            Console.WriteLine($"My name is: {name}");
+            Console.WriteLine($"My location is: {location}");
+
+            Console.WriteLine("Press any key to quit");
+            Console.ReadKey();
+        }
+    }
+}

ad48f64ecdfc70a5f083481fe0997898b4e5251a
PC <PC@BARBARA-11>
2020-05-05 13:50:39 -0600

    Added MegaDesk

---
+++ MegaDesk-Schurr/MegaDesk-Schurr/Desk.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaDesk_Schurr
+{
+    public class Desk
+    {
+
+        public enum DesktopMaterial1
+        {
+            Laminate,
+            Oak,
+            Rosewood,
+            Veneer,
+            Pine
+        }
+        public Desk() { }
+
+
+
+        public int Width { get; set; }
+
+        public int Depth { get; set; }
+
+        public int NumberOfDrawers { get; set; }
+    }
+}
---
+++ MegaDesk-Schurr/MegaDesk-Schurr/DeskQuote.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaDesk_Schurr
+{
+    public class DeskQuote
+    {
+        public DateTime QuoteTime { get; set; }
+        public string CustomerName { get; set; }
+        public Desk Desk { get; set; }
+
+        public decimal GetQuote()
+        {
+        }
+    }
+}

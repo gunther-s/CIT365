a166b67c960389304040bdd65e9bbc08cdf07e25
PC <PC@BARBARA-11>
2020-05-09 16:25:25 -0600

    Week 1 final

--- MegaDesk-Schurr/MegaDesk-Schurr/AddQuote.Designer.cs
+++ MegaDesk-Schurr/MegaDesk-Schurr/AddQuote.Designer.cs
@@ -36,18 +36,18 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
             this.label2 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
-            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label6 = new System.Windows.Forms.Label();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -96,6 +96,7 @@
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.Location = new System.Drawing.Point(172, 62);
+            this.textBox1.MaxLength = 64;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(366, 23);
             this.textBox1.TabIndex = 4;
@@ -131,6 +132,55 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Desk Size";
             // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown3.Location = new System.Drawing.Point(109, 80);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            48,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Minimum = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(53, 24);
+            this.numericUpDown3.TabIndex = 1;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            this.numericUpDown3.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown2.Location = new System.Drawing.Point(109, 20);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            96,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            24,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(55, 24);
+            this.numericUpDown2.TabIndex = 0;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            24,
+            0,
+            0,
+            0});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            this.numericUpDown2.Validating += new System.ComponentModel.CancelEventHandler(this.numericUpDown2_Validating);
+            // 
             // label2
             // 
             this.label2.AutoSize = true;
@@ -155,32 +205,30 @@
             // 
             this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.numericUpDown1.Location = new System.Drawing.Point(486, 137);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            7,
+            0,
+            0,
+            0});
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(52, 24);
             this.numericUpDown1.TabIndex = 14;
             // 
-            // numericUpDown2
-            // 
-            this.numericUpDown2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.numericUpDown2.Location = new System.Drawing.Point(109, 20);
-            this.numericUpDown2.Name = "numericUpDown2";
-            this.numericUpDown2.Size = new System.Drawing.Size(55, 24);
-            this.numericUpDown2.TabIndex = 0;
-            // 
-            // numericUpDown3
-            // 
-            this.numericUpDown3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.numericUpDown3.Location = new System.Drawing.Point(111, 80);
-            this.numericUpDown3.Name = "numericUpDown3";
-            this.numericUpDown3.Size = new System.Drawing.Size(53, 24);
-            this.numericUpDown3.TabIndex = 1;
-            // 
             // comboBox1
             // 
+            this.comboBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Append;
+            this.comboBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
             this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Laminate",
+            "Oak",
+            "Pine",
+            "Rosewood",
+            "Veneer"});
             this.comboBox1.Location = new System.Drawing.Point(486, 195);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.Sorted = true;
             this.comboBox1.TabIndex = 15;
             // 
             // label6
@@ -195,7 +243,14 @@
             // 
             // comboBox2
             // 
+            this.comboBox2.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Append;
+            this.comboBox2.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
             this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "No Rush",
+            "Rush - 3 Days",
+            "Rush - 5 Days",
+            "Rush - 7 Days"});
             this.comboBox2.Location = new System.Drawing.Point(486, 236);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(121, 21);
@@ -225,9 +280,9 @@
             this.Text = "Add Quote";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.AddQuote_FormClosed);
             this.groupBox1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- MegaDesk-Schurr/MegaDesk-Schurr/AddQuote.cs
+++ MegaDesk-Schurr/MegaDesk-Schurr/AddQuote.cs
@@ -12,6 +12,7 @@ namespace MegaDesk_Schurr
 {
     public partial class AddQuote : Form
     {
+        
         private Form _mainMenu;
 
         public AddQuote(Form mainMenu)
@@ -20,6 +21,7 @@ namespace MegaDesk_Schurr
 
             _mainMenu = mainMenu;
         }
+        
         private void button2_Click(object sender, EventArgs e)
         {
             Close();
@@ -33,6 +35,7 @@ namespace MegaDesk_Schurr
 
         private void button1_Click(object sender, EventArgs e)
         {
+            MessageBox.Show("It was detailed in the Tuesday class that THIS WEEK we just needed the interface to work (see time 1:09:56 on the video). Is that right? Who knows.");
             var desk = new Desk
             {
                 //Width = deskWidth.Value,
@@ -43,6 +46,61 @@ namespace MegaDesk_Schurr
 
         }
 
-        
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            if (numericUpDown2.Value < Desk.MIN_WIDTH)
+            {
+                numericUpDown2.Value = Desk.MIN_WIDTH;
+                MessageBox.Show("Min. width is 24in");
+            }
+            if (numericUpDown2.Value > Desk.MAX_WIDTH)
+            {
+                numericUpDown2.Value = Desk.MAX_WIDTH;
+                MessageBox.Show("Max width is 96in");
+            }
+        }
+
+        //Why is this here? See block comment below.
+        private void numericUpDown2_Validating(object sender, CancelEventArgs e)
+        {
+            if (numericUpDown2.Value < Desk.MIN_WIDTH)
+            {
+                // Cancel the event and select the text to be corrected by the user.
+                e.Cancel = true;
+                numericUpDown2.Value = Desk.MIN_WIDTH;
+                MessageBox.Show("Min. width is 24in");
+            }
+            if (numericUpDown2.Value > Desk.MAX_WIDTH)
+            {
+                e.Cancel = true;
+                numericUpDown2.Value = Desk.MAX_WIDTH;
+                MessageBox.Show("Max width is 96in");
+            }
+            else
+            {
+                e.Cancel = false;
+            }
+        }
+
+        /* At this point, the instructions say to do a KeyPress event for Depth.
+         * However, on Tuesday's class, as recorded in the video "CIT 365 - Spring 2020 - Lesson 3 Day 1" at 1:06:50,
+         * the instructor showed how to use the "Minimum" and "Maximum" property for the numeric box, thus allowing us to save "hours" of work.
+         * So, since I've already followed those directions, and added my own validation just in case, at the moment
+         * I see no reason for me to try to cobble together some KeyPress event.
+         */
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+            if (numericUpDown3.Value < Desk.MIN_DEPTH)
+            {
+                numericUpDown3.Value = Desk.MIN_DEPTH;
+                MessageBox.Show("Min. depth is 12in");
+            }
+            if (numericUpDown3.Value > Desk.MAX_DEPTH)
+            {
+                numericUpDown3.Value = Desk.MAX_DEPTH;
+                MessageBox.Show("Max depth is 48in");
+            }
+        }
     }
 }
